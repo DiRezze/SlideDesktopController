796edeb1ab8f0384c8788750d640654e41785d23
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2024-12-24 12:37:46 -0300

    Ícone adicionado

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -16,7 +16,7 @@ namespace SlideDesktopController
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
